a98ec712592c2b402671d5e7a2b0af98cc27b033
yusufaltunok <yusuf.66.77@gmail.com>
2023-05-01 00:34:27 +0300

    v3

---
+++ src/main/java/Deneme.java
@@ -0,0 +1,11 @@
+public class Deneme {
+
+    private int urunNo;
+    private String urunIsim;
+    private double urunFiyat;
+    private static int urunSayisi=0;
+
+    public static void main(String[] args) {
+
+    }
+}
